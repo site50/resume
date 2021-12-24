ee38f55ad20ce23ca50db37da21d5bfa25a89c54
rimmabogdanova23@gmail.com <dollorabel@gmail.com>
2021-12-24 20:09:59 +0300

    First commit resume

---
+++ folder1/footer.js
@@ -0,0 +1,6 @@
+
+document.getElementById("show-footer").innerHTML=
+'<footer>'
+	+'<span>@ Rimma Bogdanova<span> - Web Developer   </p>'
+	+'<p>&#9742; 89869741327 /</span>&#9993; sitebogd15@gmail.com</span></p>'
++'</footer>';
