927f9da3dc7ea5b0bb27cd239c2d6f931659a91a
Rimma <rimmabogdanova23@gmail.com>
2021-12-26 23:13:24 +0300

    Update footer.js

--- folder1/footer.js
+++ folder1/footer.js
@@ -1,6 +1,8 @@
 
+
 document.getElementById("show-footer").innerHTML=
 '<footer>'
-	+'<span>@ Rimma Bogdanova<span> - Web Developer   </p>'
-	+'<p>&#9742; 89869741327 /</span>&#9993; sitebogd15@gmail.com</span></p>'
+	+'<p>@ Rimma Bogdanova  &#9742; 89869741327</p>'
+	+'<p>  sitebogd15@gmail.com</p>'
+
 +'</footer>';
