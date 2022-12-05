ec6564fc902001a2017879aa08af6b3eca854d56
Rimma <rimmabogdanova23@gmail.com>
2022-12-05 11:25:03 +0300

    Update footer.js

--- folder1/footer.js
+++ folder1/footer.js
@@ -2,7 +2,7 @@
 
 document.getElementById("show-footer").innerHTML=
 '<footer>'
-	+'<p>@ Rimma Bogdanova  &#9742; 89869741327</p>'
+	+'<p>@ Rimma Bogdanova Telegram: Sitethree</p>'
 	+'<p>  sitebogd15@gmail.com</p>'
 
 +'</footer>';
